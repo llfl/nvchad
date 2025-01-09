94d8a9fd88fb153814e0bc1c43bee21adffa23f8
Kun(llfl) <llfl@linux.alibaba.com>
2025-01-09 15:13:43 +0800

    update lspconfig and blankindent

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -29,28 +29,37 @@ return {
       "nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope or fzf]
     },
   },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {},
+    config = function ()
+      local highlight = {
+        "CursorColumn",
+        "Whitespace",
+    }
+    require("ibl").setup {
+        -- indent = { highlight = highlight, char = "·̩̍" },
+        indent = { highlight = highlight, char = "" },
+        whitespace = {
+            highlight = highlight,
+            remove_blankline_trail = false,
+        },
+        scope = { enabled = false },
+    }
+    end,
+  },
 
-  -- {
-  --   "lukas-reineke/indent-blankline.nvim",
-  --   version = "2.20.7",
-  --   event = "User FilePost",
-  --   opts = function()
-  --     return require("plugins.configs.others").blankline
-  --   end,
-  --   config = function(_, opts)
-  --     require("core.utils").load_mappings "blankline"
-  --     dofile(vim.g.base46_cache .. "blankline")
-  --     require("indent_blankline").setup(opts)
-  --   end,
-  -- },
-  --
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
+    main = "lspconfig",
     config = function()
-      autostart = false,
-      require "configs.lspconfig"
+      local lspconfig = require "lspconfig"
+      for _, lsp in ipairs(lspconfig) do
+        lsp.setup{
+          autostart = false
+        }
+      end
     end,
   },
 
